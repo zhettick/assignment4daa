a499f9ecb85074101b26ba9a01cf9bee3e6e8c35
zhettick <belikeotall@gmail.com>
2025-11-02 22:20:22 +0500

    base

--- src/main/java/org/sabulla/graph/Edge.java
+++ src/main/java/org/sabulla/graph/Edge.java
@@ -1,4 +1,19 @@
 package org.sabulla.graph;
 
 public class Edge {
+    private final int to;
+    private final int weight;
+
+    public Edge(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public int getTo() {
+        return to;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
 }
--- src/main/java/org/sabulla/graph/Graph.java
+++ src/main/java/org/sabulla/graph/Graph.java
@@ -1,4 +1,29 @@
 package org.sabulla.graph;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Graph {
+    private final int vertices;
+    private final List<List<Edge>> adjacency;
+
+    public Graph(int vertices) {
+        this.vertices = vertices;
+        adjacency = new ArrayList<>();
+        for (int i = 0; i < vertices; i++) {
+            adjacency.add(new ArrayList<>());
+        }
+    }
+
+    public void addEdge(int u, int v, int w) {
+        adjacency.get(u).add(new Edge(v, w));
+    }
+
+    public int getVertices() {
+        return vertices;
+    }
+
+    public List<List<Edge>> getAdjacency() {
+        return adjacency;
+    }
 }
